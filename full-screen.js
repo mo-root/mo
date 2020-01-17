ebb99e4c1a595555f67e86886a757c13e0982aff
ikoursh <inbar.koursh@gmail.com>
2020-01-17 19:01:32 +0200

    Initial commit: added opening screen
    P.S
    sorry Moin, I was bored

---
+++ full-screen.js
@@ -0,0 +1,25 @@
+
+/* View in fullscreen */
+function openFullscreen() {
+  console.log("requesting full screen");
+
+  var elem = document.documentElement;
+
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+  function start() {
+    console.log("start");
+    openFullscreen();
+
+    document.getElementById("start").style.opacity = 0;
+    document.getElementById("Video").play();
+
+  }
